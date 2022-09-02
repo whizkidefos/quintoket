1ae6ba83241704d5e445f617ca24b6b9c3d56bd9
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-09-02 11:11:01 +0100

    Added all image assets

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import images from '../assets';
 
 const Navbar = () => {
+    const { theme, setTheme } = useTheme();
+    console.log({ theme })
     return (
-        <div>Navbar</div>
+        <nav className='fixed z-10 flex-row w-full p-4 bg-white border-b flexBetween dark:bg-nft-dark dark:border-nft-gray-1 border-nft-gray-1'>Navbar</nav>
     )
 }
 
