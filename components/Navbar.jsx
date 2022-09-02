e09be0aac5404b784ce18a5a1c2b7bfcdfb838a4
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-09-02 15:32:58 +0100

    added light/dark mode toggle

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,34 @@ const Navbar = () => {
     const { theme, setTheme } = useTheme();
     console.log({ theme })
     return (
-        <nav className='fixed z-10 flex-row w-full p-4 bg-white border-b flexBetween dark:bg-nft-dark dark:border-nft-gray-1 border-nft-gray-1'>Navbar</nav>
+        <nav className='fixed z-10 flex-row w-full p-4 bg-white border-b flexBetween dark:bg-nft-dark dark:border-nft-gray-1 border-nft-gray-1'>
+            <div className='flex flex-row justify-start flex-1'>
+                <Link href='/'>
+                    <div className='flex cursor-pointer flexCenter md:hidden' onClick={() => { }}>
+                        <Image src={images.logo02} objectFit='contain' width={32} alt='logo' />
+                        <p className='ml-1 text-lg font-semibold dark:text-white text-nft-black-1'>QuintoKet</p>
+                    </div>
+                </Link>
+                <Link href='/'>
+                    <div className='hidden md:flex' onClick={() => { }}>
+                        <Image src={images.logo02} objectFit='contain' width={32} alt='logo' />
+                    </div>
+                </Link>
+            </div>
+            <div className='flex flex-row justify-end flex-initial'>
+                <div className='flex items-center mr-2'>
+                    <input type='checkbox' className='checkbox' id='checkbox' onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')} />
+                    <label htmlFor='checkbox' className='relative w-8 h-4 p-1 bg-black flexBetween rounded-2xl label'>
+                        <i className='fas fa-sun' />
+                        <i className='fas fa-moon' />
+                        <div className='absolute w-3 h-3 bg-white rounded-full ball' />
+                    </label>
+                </div>
+            </div>
+            <div className='flex md:hidden'>
+                MenuItems
+            </div>
+        </nav>
     )
 }
 
